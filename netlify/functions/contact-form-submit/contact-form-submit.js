5f5308d4d907d8c2b1b3d06961c569408e84fafc
funstra <founstra@gmail.com>
2021-08-24 15:40:09 +0200

    form, netlify function

---
+++ netlify/functions/contact-form-submit/contact-form-submit.js
@@ -0,0 +1,64 @@
+// Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
+const nodemailer = require('nodemailer')
+const handler = async (event) => {
+
+    const emailPattern = /^[\w]{1,}[\w.+-]{0,}@[\w-]{2,}([.][a-zA-Z]{2,}|[.][\w-]{2,}[.][a-zA-Z]{2,})$/
+
+    try {
+        const data = JSON.parse(event.body)
+        if (emailPattern.test(data.email)) {
+            const transporter = nodemailer.createTransport({
+                host: "digimoon.space",
+                port: 465,
+                secure: true,
+                auth: {
+                    user: process.env.CONTACT_FORM_EMAIL,
+                    pass: process.env.CONTACT_FORM_PWD
+                }
+            })
+            const info = await transporter.sendMail({
+                from: data.email,
+                to: 'contact@digimoon.space',
+                subject: data.subject || 'no subject',
+                text: data.body
+            })
+            console.log(info.accepted)
+            if ((info).accepted) {
+                console.log((info).messageId)
+                console.log((info).accepted)
+                const status = (info).response
+                console.log(status)
+                return {
+                    statusCode: 250,
+                    body: JSON.stringify({ statusCode: 250, message: `Email sent!` }),
+                }
+            } else if ((info).rejected) {
+                console.log('here')
+
+                const status = (info).response
+                console.log(status, "transport rejected")
+                return {
+                    statusCode: 500,
+                    body: JSON.stringify({ statusCode: 500, message: `Email not sent!` }),
+                }
+            }
+
+        } else {
+            return {
+                statusCode: 400,
+                body: JSON.stringify({ statusCode: 400, message: `Not a valid email` }),
+            }
+        }
+
+    }
+    catch (error) {
+        console.log('WHAT')
+        console.log(error.response)
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ statusCode: 500, message: `Not a valid email`, body: error.toString() })
+        }
+    }
+}
+
+module.exports = { handler }
